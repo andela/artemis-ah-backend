191f8816533c32d2acb0184500004c021ddfb848
Daniel Austin <danielaustin718@gmail.com>
2019-02-27 14:48:21 +0100

    chore(createArticles): feature to create articles
    - Created migrations and models
    - Created Controller class with helper functions for responses to prevent repetition
    
    [Delivers #164198494]

---
+++ controllers/controller.js
@@ -0,0 +1,79 @@
+/**
+ * Base controller class. All controller classes should extend this class
+ */
+class Controller {
+  response(res) {
+    return {
+      /**
+       * Sends a status 403 and `data` to the client.
+       * Should be sent when a request may be valid but the user 
+       * doesn't have permissions to perform the action.
+       * 
+       * @param data
+       */
+      forbidden(message) {
+        this.sendMessage(403, message);
+      },
+
+      /**
+       * Sends a status 401 and `data` to the client.
+       * Should be sent when a request required authentication but it is not provided.
+       * 
+       * @param data 
+       */
+      unauthorized(message) {
+        this.sendMessage(401, message);
+      },
+
+      /**
+       * Sends a status 200 and `data` to the client.
+       * 
+       * 
+       * @param data 
+       */
+      success(data) {
+        this.sendData(200, data);
+      },
+
+      /**
+       * Sends a status 404 and `data` to the client.
+       * Should be sent when a resource can't be found to fulfill the request.
+       * 
+       * @param data
+       */
+      notFound(message) {
+        this.sendMessage(404, message);
+      },
+
+      /**
+       * Send data to the client.
+       * 
+       * @access private
+       * @param {integer} status 
+       * @param {*} data 
+       */
+      sendData(status, data) {
+        res.status(status).json({
+          status,
+          data,
+        });
+      },
+
+      /**
+       * Send a message back to the client.
+       * 
+       * @access private
+       * @param {integer} status 
+       * @param {*} message 
+       */
+      sendMessage(status, message) {
+        res.status(status).json({
+          status,
+          message,
+        });
+      }
+    };
+  }
+}
+
+export default Controller;
---
+++ database/migrations/20190227133849-create-article.js
@@ -0,0 +1,50 @@
+export default {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Articles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      body: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      primaryImageUrl: {
+        type: Sequelize.STRING,
+      },
+      totalClaps: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
+      slug: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      }
+    });
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Articles');
+  }
+};
