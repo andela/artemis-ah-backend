0df03b84c9b90e89b976aeca7f553df57fc1bf6b
Ayo-Oluwa Adebayo <ayo-oluwaadebayo@Ayo-Oluwas-MacBook-Pro.local>
2019-03-02 11:48:06 +0100

    feat(sendgrid): Reset password via email - send reset password link to user's email - link should authenticate user email and update table with new password

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,5 +5,7 @@ const authRoute = express.Router();
 
 authRoute.post('/users', Users.signupUser);
 authRoute.get('/users/verifyemail', Users.verifyUserEmail);
+authRoute.post('/users/reset-password', Users.resetPasswordEmail);
+authRoute.patch('/users/reset-password', Users.resetPassword);
 
 export default authRoute;
