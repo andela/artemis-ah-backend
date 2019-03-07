0b26463f274cc5263baaca806d3735e40ad861f7
Andela <andela@Andelas-MacBook-Pro.local>
2019-03-07 16:25:15 +0100

    update article clap

--- server/controllers/articleClap.js
+++ server/controllers/articleClap.js
@@ -2,13 +2,37 @@ import dotenv from 'dotenv';
 import db from '../database/models';
 import { response } from '../utils';
 
-const { Article, ArticleClap } = db;
+const { ArticleClap, Article } = db;
 dotenv.config();
 
 /**
  * @description Class to implement clap and unclap
  */
 export default class ArticleClapToggle {
+  /**
+   * @description increaments total claps
+   * @param {number} totalClaps
+   * @param {number} articleId
+   * @returns {nothing} err
+   */
+  static async incrementTotalClaps(totalClaps, articleId) {
+    const newTotalClaps = totalClaps + 1;
+    await Article.update({ totalClaps: newTotalClaps },
+      { where: { id: articleId } });
+  }
+
+  /**
+   * @description decreases total claps
+   * @param {number} totalClaps
+   * @param {number} articleId
+   * @returns {nothing} err
+   */
+  static async decreaseTotalClaps(totalClaps, articleId) {
+    const newTotalClaps = totalClaps - 1;
+    await Article.update({ totalClaps: newTotalClaps },
+      { where: { id: articleId } });
+  }
+
   /**
      * @description claps and retrieves claap from articles
      * @param {object} req
@@ -17,26 +41,29 @@ export default class ArticleClapToggle {
      */
   static async clapToggle(req, res) {
     const { id } = req.user;
-    const { slug } = req.params;
+    const { article } = req;
 
-    const getArticle = await Article.findOne({ where: { slug } });
-
-    if (!getArticle) response(res).notFound({ message: 'article not found' });
-
-    else if (getArticle.userId === id) response(res).forbidden({ message: 'you cannot clap for your own article' });
-
-    else {
+    // heck if current user is author
+    if (article.userId === id) {
+      response(res).forbidden({ message: 'you cannot clap for your own article' });
+    } else {
+      // Add a clap
       const clap = await ArticleClap.findOrCreate({
-        where: { userId: id, articleId: getArticle.id },
+        where: { userId: id, articleId: article.id },
         defaults: { clap: true }
       });
 
-      if (clap[1] === true) response(res).created({ message: 'you just clapped for this article' });
+      if (clap[1] === true) {
+        // Increament total claps on article
+        await ArticleClapToggle.incrementTotalClaps(article.totalClaps, article.id);
 
-      else {
+        response(res).created({ message: 'you just clapped for this article' });
+      } else {
         await ArticleClap.destroy({
-          where: { userId: id, articleId: getArticle.id }
+          where: { userId: id, articleId: article.id }
         });
+        // Decrease total claps on article
+        await ArticleClapToggle.decreaseTotalClaps(article.totalClaps, article.id);
 
         response(res).success({ message: 'you just retrieved your clap' });
       }
